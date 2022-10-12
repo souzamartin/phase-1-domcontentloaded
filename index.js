5fff74e9487a2b438a079633f63438e4a1ff2c54
Martin <souzam@sonic.net>
2022-10-12 16:44:37 -0400

    commit tree 4daf3abaf397c945f296af1e4f0cbfcb7433e979

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Your code goes here
+document.addEventListener('DOMContentLoaded', function(){
+    const pRename = document.getElementById("text");
+    pRename.textContent = "This is really cool!";
+})
